f28cf5c7ace45ef06f8d8247ad3e6e2d2b0c5371
Mhoncorvo <acmurilohenderson@gmail.com>
2025-08-28 20:11:23 -0300

    Ideia inicial

---
+++ mais.C
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main () {
+
+int pontoBom;
+int pontoNeutro;
+
+
+
+
+return 0;
+}
+
+
+void resultado() {
+     if (pontoBom > pontoRuim && pontoBom > pontoNeutro || pontoBom === pontoNeutro) {
+       finalBom();
+} else if (pontoNeutro > pontoBom && pontoNeutro > pontoRuim || pontoBom === pontoRuim) {
+
+} else if (pontoRuim > pontoNeutro && pontoRuim > pontoBom || pontoRuim === pontoNeutro) {
+
+}
+
+
+void receberResposta() {
+
+
+}
+
+
+void textoIntroducao() {
+
+
+}
+
+void finalBom() {
+      printf(".........."):
+ 
+
+}
